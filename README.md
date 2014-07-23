71c26baadc15a94a813ec6aecbc4ee525c237205
Cale J Spencer <calejspencer@gmail.com>
2014-07-23 17:23:28 -0400

    Initial commit of local app + Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+Footprints
+==========
+
+See the places you've been!
+
+## DESCRIPTION
+
+Connects to Foursquare (and possibly other social apps) and pulls location / check-in history.
+
+– User signs in with Foursquare credentials to access history. 
+– Pulls in user's history from Foursquare.
+– Stores history in local DB.
+– Refreshes history into local DB.  
+
+## INSTALLATION
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Footprints::Application.config.secret_key_base = '64087ddf67771818e026b822278a419d3c96651128aeaa22bf3a3eb335b95ff3c9caa84e058a870e408ddbf1acffed944b746ea70b0d46a33cfa647f074b0400'
